68d11055951340ac4efab62b1378fa3c96906764
Aleksey Pasechnik <Arhellist@mail.ru>
2022-05-18 23:14:45 +0400

    добавлен index.html, расширены все страницы

--- JS/index.js
+++ JS/index.js
@@ -4,28 +4,6 @@
 (function($) {
     "use strict";
 
-    //Parallax            
-
-    function scrollBanner() {
-        $(document).on('scroll', function() {
-            var scrollPos = $(this).scrollTop();
-            $('.parallax-fade-top').css({
-                'top': (scrollPos / 2) + 'px',
-                'opacity': 1 - (scrollPos / 700)
-            });
-            $('.parallax-00').css({
-                'top': (scrollPos / -3.5) + 'px'
-            });
-            $('.parallax-01').css({
-                'top': (scrollPos / -2.8) + 'px'
-            });
-            $('.parallax-top-shadow').css({
-                'top': (scrollPos / -2) + 'px'
-            });
-        });
-    }
-    scrollBanner();
-
     //Page cursors
 
     document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
@@ -56,54 +34,41 @@
         t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
     }
 
+    //Navigation
 
-    //Scroll back to top
+    var app = function() {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.nav__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener('click', function() {
+                return toggleClass(body, 'nav-active');
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass)) element.classList.remove(stringClass);
+            else element.classList.add(stringClass);
+        };
+        init();
+    }();
 
-    $(document).ready(function() {
-        var offset = 300;
-        var duration = 400;
-        jQuery(window).on('scroll', function() {
-            if (jQuery(this).scrollTop() > offset) {
-                jQuery('.scroll-to-top').addClass('active-arrow');
-            } else {
-                jQuery('.scroll-to-top').removeClass('active-arrow');
-            }
-        });
-        jQuery('.scroll-to-top').on('click', function(event) {
-            event.preventDefault();
-            jQuery('html, body').animate({ scrollTop: 0 }, duration);
-            return false;
-        })
 
+    //Switch light/dark
 
-        /* Hero Case study images */
-
-        $('.case-study-name:nth-child(1)').on('mouseenter', function() {
-            $('.case-study-name.active').removeClass('active');
-            $('.case-study-images li.show').removeClass("show");
-            $('.case-study-images li:nth-child(1)').addClass("show");
-            $('.case-study-name:nth-child(1)').addClass('active');
-        })
-        $('.case-study-name:nth-child(2)').on('mouseenter', function() {
-            $('.case-study-name.active').removeClass('active');
-            $('.case-study-images li.show').removeClass("show");
-            $('.case-study-images li:nth-child(2)').addClass("show");
-            $('.case-study-name:nth-child(2)').addClass('active');
-        })
-        $('.case-study-name:nth-child(3)').on('mouseenter', function() {
-            $('.case-study-name.active').removeClass('active');
-            $('.case-study-images li.show').removeClass("show");
-            $('.case-study-images li:nth-child(3)').addClass("show");
-            $('.case-study-name:nth-child(3)').addClass('active');
-        })
-        $('.case-study-name:nth-child(4)').on('mouseenter', function() {
-            $('.case-study-name.active').removeClass('active');
-            $('.case-study-images li.show').removeClass("show");
-            $('.case-study-images li:nth-child(4)').addClass("show");
-            $('.case-study-name:nth-child(4)').addClass('active');
-        })
-        $('.case-study-name:nth-child(1)').trigger('mouseenter')
-
+    $("#switch").on('click', function() {
+        if ($("body").hasClass("light")) {
+            $("body").removeClass("light");
+            $("#switch").removeClass("switched");
+        } else {
+            $("body").addClass("light");
+            $("#switch").addClass("switched");
+        }
     });
 
 })(jQuery);
